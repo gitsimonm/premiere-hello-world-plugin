049b763a9235a012178b790c61281d46911cd454
gitsimonm <19911322+gitsimonm@users.noreply.github.com>
2024-03-28 18:25:31 +0000

    init

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// user launches the plugin from Premiere
+
+// this is the launch script for your plugin 
+
+// UI for the plugin panel 
+
---
+++ jsx/premiere.jsx
@@ -0,0 +1,11 @@
+// Extendscript 
+
+// tell Premiere to open a project
+// save the file
+// export
+
+// scripting to replace user interaction // control Premiere Pro
+
+
+
+
